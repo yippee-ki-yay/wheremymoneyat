36a5dd71acc783bc4fcc84dbb176886f7ebb4321
Milan Keca <mkeca@renderedtext.com>
2017-02-02 16:50:55 +0100

    Add template for login component

--- client/src/login/Login.js
+++ client/src/login/Login.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Login extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
 
   render() {
     return (
-      <h1>Login</h1>
-    )
+      <div>
+        <h1>Login</h1>
+
+        <div className="form-group">
+          <input type="text" value="" placeholder="Username" />
+          <input type="password" value="" placeholder="Password" />
+          <button className="btn btn-default">Login</button>
+        </div>
+
+      </div>
+    );
+  }
+
+  login = () => {
+    console.log(this.state.username);
+    console.log(this.state.password);
   }
 }
 
