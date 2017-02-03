36e5b99efc48f0a66497dc9564aca0520ca5f778
nenad <nesa993@gmail.com>
2017-02-03 14:34:39 +0100

    creating an entry, parsing it and sending it to the api

--- client/src/entry/Entry.js
+++ client/src/entry/Entry.js
@@ -2,30 +2,63 @@ import React, { Component } from 'react';
 import './Entry.css';
 import axios from 'axios';
 
+import decode from 'jwt-decode';
+
 class Entry extends Component {
 
   constructor(props) {
     super(props);
+    this.jwt = localStorage.getItem('wheremymoneyat-jwt');
+    this.user = decode(this.jwt);
 
-    this.state = {text: ''};
+    this.state = {entryText: ''};
   }
 
-  addEntry() {
-    console.log(`Entry added `);
+  addEntry = () => {
 
-    axios.get('http://localhost:6969/api/test')
+    const entry = {
+      text: this.state.entryText,
+      author: this.user._id
+    };
+
+    // Extract price, for now just gets a number if exists
+    const prices = entry.text.match(/\d+/);
+
+    entry.price = (prices.length > 0) ? (+prices[0]) : 0;
+
+    // Extract hashtags again for now very simple
+    entry.tags = entry.text.match(/#[\w]+(?=\s|$)/g);
+
+    axios.post('http://localhost:6969/api/entries', entry, {
+      headers: {
+        Authorization: 'Bearer ' + this.jwt
+      }
+    })
     .then((resp) => {
       console.log(resp);
+      this.setState({
+        entryText: ''
+      });
     });
   }
 
   render() {
     return (
-      <div className="main-form">
-        <input type="text"  className="big-input"></input>
-        <button type="button" className="entry-btn" onClick={this.addEntry}>Spent</button>
+      <div className="row">
+        <div className="input-group big-input col-md-8 col-md-offset-2">
+          <input type="text" onChange={ this.onEntryChange } value={ this.state.entryText } className="form-control" />
+          <span className="input-group-btn">
+            <button className="btn btn-default" onClick={ this.addEntry } type="button">Spent</button>
+          </span>
+        </div>
       </div>
     )
+  }
+
+  onEntryChange = (e) => {
+    this.setState({
+      entryText: e.target.value
+    });
   }
 
 }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const jwt = require('express-jwt');
+
 const entryController = require('../controllers/entry.controller');
 const userController = require('../controllers/user.controller');
 
+const auth = jwt({
+  secret: process.env.JWT_SECRET,
+});
+
 // Test route
 router.get('/test', (req, res) => {res.send("wrks")});
+router.get('/test-auth', auth, (req, res) => {res.send(req.user)});
 
 //Entries
 router.post('/entries', entryController.addEntry);
